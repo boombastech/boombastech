42f7f578ebebb9ae962f4449a39436984218215f
Phil Armstrong <phil@boombastech.co.uk>
2015-12-17 16:43:52 +0000

    removed HtmlController as now redundant with static content working

--- photos/src/main/java/uk/co/boombastech/photos/injection/PhotosModule.java
+++ photos/src/main/java/uk/co/boombastech/photos/injection/PhotosModule.java
@@ -2,19 +2,19 @@ package uk.co.boombastech.photos.injection;
 
 import com.google.common.collect.Lists;
 import com.google.inject.TypeLiteral;
-import uk.co.boombastech.photos.controllers.HtmlController;
-import uk.co.boombastech.photos.models.Photo;
 import uk.co.boombastech.photos.controllers.DeleteAllController;
 import uk.co.boombastech.photos.controllers.HomepageController;
 import uk.co.boombastech.photos.importer.ImporterController;
 import uk.co.boombastech.photos.importer.ImporterService;
 import uk.co.boombastech.photos.importer.PhotoIndexer;
+import uk.co.boombastech.photos.models.Photo;
 import uk.co.boombastech.photos.search.PhotoFacets;
 import uk.co.boombastech.routes.MutableRouteStore;
-import uk.co.boombastech.routes.RouteBuilder;
 import uk.co.boombastech.solr.search.Facets;
 import uk.co.boombastech.solr.search.SolrService;
 import uk.co.boombastech.web.WebModule;
+
+import static uk.co.boombastech.routes.RouteBuilder.route;
 
 public class PhotosModule extends WebModule {
 
@@ -24,10 +24,9 @@ public class PhotosModule extends WebModule {
 
 	@Override
 	protected void configure() {
-		mutableRouteStore.withRoute(RouteBuilder.route("/").withController(HtmlController.class));
-		mutableRouteStore.withRoute(RouteBuilder.route("/rest").withController(HomepageController.class));
-		mutableRouteStore.withRoute(RouteBuilder.route("/import").withController(ImporterController.class));
-		mutableRouteStore.withRoute(RouteBuilder.route("/delete-all").withController(DeleteAllController.class));
+		mutableRouteStore.withRoute(route("/rest").withController(HomepageController.class));
+		mutableRouteStore.withRoute(route("/import").withController(ImporterController.class));
+		mutableRouteStore.withRoute(route("/delete-all").withController(DeleteAllController.class));
 
 		bind(new TypeLiteral<SolrService<Photo>>() {}).toProvider(PhotoSolrServiceProvider.class);
 
