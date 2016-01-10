b4b0278618a9192905a3846c7d6755a561fc27f7
Phil Armstrong <phil.armstrong1@gmail.com>
2016-01-10 22:14:31 +0000

    front end photo viewer alignment and individual photo updating

--- photos/src/main/web/javascript/controllers/photo-viewer-controller.js
+++ photos/src/main/web/javascript/controllers/photo-viewer-controller.js
@@ -1,6 +1,15 @@
 angular.module('photoApp')
-	.controller('PhotoViewerController', ['$scope', '$uibModalInstance', 'photo', function($scope, $uibModalInstance, photo) {
+	.controller('PhotoViewerController', ['$scope', '$uibModalInstance', 'PhotoService','photo', function($scope, $uibModalInstance, photoService, photo) {
 		$scope.photo = photo;
+
+		$scope.save = function() {
+			photoService.update([photo], function(response) {
+				console.log('success');
+			},
+			function(response) {
+				console.log('error');
+			})
+		}
 
 		$scope.cancel = function() {
 			$uibModalInstance.dismiss('cancel');
